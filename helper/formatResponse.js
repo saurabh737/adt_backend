b8816952c0c2f3f34f2ad68fd23b5c672a5e3ed0
Kumar Saurabh <kumarsaurabh737@gmail.com>
2022-04-26 13:41:01 -0400

    fix

--- helper/formatResponse.js
+++ helper/formatResponse.js
@@ -5,24 +5,22 @@ async function format(user_id, data, userFav) {
         try {
             let recipeIdlist = [];
             if (!userFav) {
-
                 let recipeIds = await db.getFavouriteIds(parseInt(user_id));
                 recipeIds = JSON.parse(JSON.stringify(recipeIds))
                 for (let j = 0; j < recipeIds.length; j++) {
                     recipeIdlist.push(recipeIds[j].recipe_Id)
                 }
             }
-            if (userFav || recipeIdlist.length > 0) {
-                for (let i = 0; i < data.length; i++) {
-                    var hours = Math.floor(data[i].cooktime / 60);
-                    var minutes = data[i].cooktime % 60;
-                    data[i].time = `${hours} hr ${minutes} min`;
-                    data[i].avg_rating = parseFloat(data[i].avg_rating.toFixed(2));
-                    if (userFav || recipeIdlist.indexOf(data[i].recipe_id) > -1) {
-                        data[i].fav = true
-                    } else {
-                        data[i].fav = false
-                    }
+            
+            for (let i = 0; i < data.length; i++) {
+                var hours = Math.floor(data[i].cooktime / 60);
+                var minutes = data[i].cooktime % 60;
+                data[i].time = `${hours} hr ${minutes} min`;
+                data[i].avg_rating = parseFloat(data[i].avg_rating.toFixed(2));
+                if (userFav || recipeIdlist.indexOf(data[i].recipe_id) > -1) {
+                    data[i].fav = true
+                } else {
+                    data[i].fav = false
                 }
             }
             resolve(data)
